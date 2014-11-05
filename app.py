8f4474bc8086af724ab8751b9500ac8e91deb273
Brian Guo <helen@helens-air.home>
2014-11-05 01:50:42 -0500

    app

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from pymongo import MongoClient
 client = MongoClient()
 db = client.chrisndbrian
 accounts = db.one
-profile = db.two
+profiles = db.two
+post_id = 1
 
 app = Flask(__name__)
 @app.route("/")
@@ -37,22 +38,48 @@ def failedlogin():
 def failedregister():
 	return render_template("failedregister.html")
 
+
 @app.route("/index", methods = ['GET','POST'])
 def index():
 	if request.method == 'POST':
 		username = request.form['username']
 		password = request.form['password']
-
 		p = accounts.find_one({"username": username})
-		if p != None:
+		if username=="" or password == "":
+			return redirect('/failedlogin')
+		elif p!=None:
 			if p["password"] == password:
+				global post_id
+				post_id = p['_id']
+				print "MISSION SUCCESS"
+				print post_id
 				return render_template("index.html")
 				#render_template();"""
 			else:
-				return render_template("login.html")
+				return redirect('/failedlogin')
 		else:
 			return redirect('/failedlogin')
-			
+
+@app.route("/profile",methods=['GET','POST'])
+def profile():
+	if request.method == 'POST':
+		p = accounts.find_one({"_id":post_id})
+		#if p==None:
+			#print True
+			#newprof = {"_id":post_id}#, #"hobby":request.form['hobby']}#, "movie":request.form["movie"], "quote":request.form["quote"],"OptionRadios":request.form["OptionRadios"],"OptionRadios2":request.form["OptionRadios2"]"""}
+			#profiles.insert(newprof)
+		print "TRUE PART 2------------------------"
+		username = p['username']
+		print "TRUE PART 3------------------------"
+		a = request.form['OptionRadios']
+		print request.form
+		return render_template("profile.html",username=username,movie=request.form.get('movie',None))#,OptionRadios=request.form['OptionRadios'])#,OptionRadios2=request.form["OptionRadios2"],hobby=request.form["hobby"],quote=request.form["quote"])
+
+
+
+@app.route("/About")
+def About():
+	return render_template("About.html")
 
 if (__name__) == "__main__":
 	app.debug = True
