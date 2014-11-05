9fc67ff8792a19555fb16cb8ab5ef5ed7cb9dd6c
Brian Guo <helen@Helens-MacBook-Air.local>
2014-11-04 19:04:29 -0500

    added everything

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.chrisndbrian
+accounts = db.one
+
+app = Flask(__name__)
+@app.route("/")
+@app.route("/login", methods = ['GET','POST'])
+def login():
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+
+		p = accounts.find_one({"username": username})
+		if p != None :
+			if p["password"] == password:
+				render_template()
+
+
+
+if (__name__) == "__main__":
+	app.debug = True
+	app.run()
